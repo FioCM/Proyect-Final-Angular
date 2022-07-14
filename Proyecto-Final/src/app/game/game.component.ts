b375e9d2cbea76c27312fae815e10382ffbd84a3
Ashley Castillo <afiocastimo02@gmail.com>
2022-07-14 02:25:03 -0600

    Edicion de componente game y uso de input
    Volví a hacer desde un inicio todo el componente "game" para que se vean estéticamente bien y sea funcional más adelante con el servicio y el API.
    
    También se agregó el uso del @Input en todos los componente en los cuales es necesario agregarlo para su correcto funcionamiento con el Servicio y con la API.

--- Proyecto-Final/src/app/game/game.component.ts
+++ Proyecto-Final/src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-game',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
+
+  @Input () juego:string="igwyevoec"
+  @Input () fecha:string="eoulvweq"
+  @Input () tipoJuego:string="ehfnfjhfuh"
+  @Input () barra1:string="hfyr8ee"
+  @Input () barra2:string="jdjdhdhd"
+  @Input () plataformas:string="hdhdhdh"
+  @Input () publishers:string="jviwevywrv"
+  @Input () websites:any="ibviwrbgivywgrb"
+  @Input () acercaJuego:string="iwlebebqiebqiyrbqivbieqyvbqeiyv"
 
   constructor() { }
 
